7b889bd25e4da820bb46f18fa3e04a72cfa1760e
stefi23 <stefania.rosca2309@gmail.com>
2020-01-07 14:43:53 +0100

    Added 0 before seconds, minutes and hours if less than 10

--- src/Components/CurrentDateAndTime.js
+++ src/Components/CurrentDateAndTime.js
@@ -1,70 +1,76 @@
-import React, { PureComponent } from 'react'
-import './CurrentDateAndTime.css';
-
+import React, { PureComponent } from "react";
+import "./CurrentDateAndTime.css";
 
 class CurrentDateAndTime extends PureComponent {
-    // state = {
-    //     date: 'day',
-    //     time: 'Now'
-    // }
-    constructor(props){
-        super(props);
-       this.state = {
-        date: 'day',
-        time: 'Now'
-       }
-    
-    }
-    
-    componentDidMount() {
-        this.timerID = setInterval(
-          () => this.tic(),
-          1000
-        );
+  // state = {
+  //     date: 'day',
+  //     time: 'Now'
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: "day",
+      time: "Now"
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tic(), 1000);
+  }
+
+  tic = () => {
+    const now = new Date();
+
+    this.setState(() => {
+      const date = now.getDate();
+      const month = now.getMonth() + 1;
+      const year = now.getFullYear();
+      let hours = now.getHours();
+      let minutes = now.getMinutes();
+      let seconds = now.getSeconds();
+
+      if (seconds < 10) {
+        seconds = "0" + now.getSeconds();
+      }
+      if (minutes < 10) {
+        minutes = "0" + now.getMinutes();
+      }
+      if (hours < 10) {
+        hours = "0" + now.getHours();
       }
 
-    tic = () => {
-       this.setState(() => {
-        const now = new Date ();
-        const date = now.getDate();
-        const month = now.getMonth() + 1;
-        const year = now.getFullYear();
-        const hours = now.getHours();
-        const minutes = now.getMinutes();
-        const seconds = now.getSeconds();
-        const day = now.getDay();
-        const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      const day = now.getDay();
+      const weekDays = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
 
+      return {
+        date: `${weekDays[day]}| ${date}/${month}/${year}`,
+        time: `${hours}:${minutes}:${seconds}`
+      };
+    });
+  };
 
-        return {
-            date: `${weekDays[day]}| ${date}/${month}/${year}`,
-            time: `${hours}:${minutes}:${seconds}`
-        }
-        // day/month/year
-       }) 
-    }
-    
-//day / month / year
-// current live hour and minutes
-
-    render(){
-        const {date,time} = this.state;
-        return(
-            <div className="pt-5">
-                <div className='row'>
-                <div className='col-6 offset-3 date'>
-                {date}  
-                </div>
-                </div>
-                <div className='row'>
-                <div className='col-6 offset-3 text-white time'>
-                {time}    
-                </div>
-                </div>
-                <hr size="30" color="white" /> 
-            </div>
-        )
-    }
-};
+  render() {
+    const { date, time } = this.state;
+    return (
+      <div className="pt-5">
+        <div className="row">
+          <div className="col-6 offset-3 date">{date}</div>
+        </div>
+        <div className="row">
+          <div className="col-6 offset-3 text-white time">{time}</div>
+        </div>
+        <hr size="30" color="white" />
+      </div>
+    );
+  }
+}
 
 export default CurrentDateAndTime;
--- src/Components/QuoteGenerator.js
+++ src/Components/QuoteGenerator.js
@@ -1,68 +1,70 @@
-import React, { Component } from 'react'
-import './QuoteGenerator.css';
-
+import React, { Component } from "react";
+import "./QuoteGenerator.css";
 
 const quotes = [
-    ' "When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love.." Marcus Aurelius ',
+  ' "When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love.." Marcus Aurelius ',
 
-    '"Courage doesn’t mean you don’t get afraid. Courage means you don’t let fear stop you." Bethany Hamilton',
+  '"Courage doesn’t mean you don’t get afraid. Courage means you don’t let fear stop you." Bethany Hamilton',
 
-    '"Before an intimidating conversation, I always remind myself that I will be the same person before and after the conversation, no matter the outcome." Olivia Fay',
-  
-  '“Life shrinks or expands in proportion to one s courage.” – Anaïs Nin',
-  
+  '"Before an intimidating conversation, I always remind myself that I will be the same person before and after the conversation, no matter the outcome." Olivia Fay',
+
+  "“Life shrinks or expands in proportion to one s courage.” – Anaïs Nin",
+
   '"The time is always right to do what is right." —Martin Luther King Jr.',
-  '“The only truly safe thing you can do is to try over and over again. To go for it, to get rejected, to repeat, to strive, to wish. Without rejection there is no frontier, there is no passion, and there is no magic.” ― James Altucher, Choose Yourself'
-
+  "“The only truly safe thing you can do is to try over and over again. To go for it, to get rejected, to repeat, to strive, to wish. Without rejection there is no frontier, there is no passion, and there is no magic.” ― James Altucher, Choose Yourself",
+  `“We are what we repeatedly do. Excellence, then, is not an act, but a habit.” – Aristotle`,
+  `“The journey of a thousand miles begins with one step.” – Lao Tzu`,
+  `“In the long run, the sharpest weapon of all is a kind and gentle spirit.” 
+― Anne Frank, The Diary of Anne Frank`
 ];
 
 class QuoteGenerator extends Component {
-constructor(props){
-super(props)
-this.state = {
-quoteGenerated: '',
-i: 1
-}
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      quoteGenerated: "",
+      i: 1
+    };
+  }
 
-generateQuote = () => {
-this.setState(( {i} ) => {
-if(i === quotes.length){
-i = 0;
-}
+  generateQuote = () => {
+    this.setState(({ i }) => {
+      if (i === quotes.length) {
+        i = 0;
+      }
 
-const quoteGenerated = quotes[i]
-i++;
-    
-return {
-i,
-quoteGenerated,
-}
+      const quoteGenerated = quotes[i];
+      i++;
 
-});
-}
+      return {
+        i,
+        quoteGenerated
+      };
+    });
+  };
 
-render(){
-const {quoteGenerated, i} = this.state
-// const quoteGenerated = this.state.quoteGenerated;
+  render() {
+    const { quoteGenerated, i } = this.state;
+    // const quoteGenerated = this.state.quoteGenerated;
 
-return(
-<div className="content text-center position-relative">
-   
-    <div className="row ">
-    <div className="col-10 offset-1 p-4 rounded-corners" >
-<div className="mb-4 text-white quote">{quoteGenerated ? quoteGenerated : quotes[0]}</div>
-        <button className="btn button !important" 
-         onClick ={this.generateQuote}>Generate Quote       
-        </button>
+    return (
+      <div className="content text-center position-relative">
+        <div className="row ">
+          <div className="col-10 offset-1 p-4 rounded-corners">
+            <div className="mb-4 text-white quote">
+              {quoteGenerated ? quoteGenerated : quotes[0]}
+            </div>
+            <button
+              className="btn button !important"
+              onClick={this.generateQuote}
+            >
+              Generate Quote
+            </button>
+          </div>
+        </div>
       </div>
-      </div> 
-</div>
-
-)
+    );
+  }
 }
-
-}
-
 
 export default QuoteGenerator;
