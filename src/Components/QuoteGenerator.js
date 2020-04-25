8aaadb187f20f3279a4ba3972720044b7d67c7bf
stefi23 <stefania.rosca2309@gmail.com>
2020-04-25 23:04:50 +0200

    cleaning code

--- src/Components/QuoteGenerator.js
+++ src/Components/QuoteGenerator.js
@@ -44,7 +44,6 @@ class QuoteGenerator extends Component {
       if (i === quotes.length) {
         i = 0;
       }
-      console.log(quotes.length);
       const quoteGenerated = quotes[i];
       i++;
 
